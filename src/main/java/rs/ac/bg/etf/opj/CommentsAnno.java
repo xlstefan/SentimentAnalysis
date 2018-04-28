25d9f4c9bdf96a1a9016f54abe74fd6058d1062b
Marko Kastratovic <marko.kastratovic@zuehlke.com>
2018-04-28 22:07:33 +0200

    aditional refactorings, adding score works by selecting apropriate value, not needed aditional button click

--- src/main/java/rs/ac/bg/etf/opj/CommentsAnno.java
+++ src/main/java/rs/ac/bg/etf/opj/CommentsAnno.java
@@ -1,44 +1,23 @@
 package rs.ac.bg.etf.opj;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
-
-public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileListner {
+public class CommentsAnno extends JFrame {
 
 	// Interface strings
 	private String txtFileExtensionString = "TXT file";
@@ -48,22 +27,18 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
 	private String lineLabelString2 = " of ";
 	private String scoredString = "Scored: ";
 	private String unscoredString = "Unscored: ";
-	private String assignScoreButtonString = "Assign score";
 	private String eraseScoreButtonString = "Erase score";
 	private String saveButtonString = "Save data to file";
 	private String jumpToNextPairCheckboxString = "Automatically jump to the next pair";
 	private String dataSavedMessageString = "Data saved!";
 
 	// Constants
-    private static final String [] scoreStrings = {"?","0","1","2","3","4","5"};
+    private static final List<String> scoreList = Arrays.asList("?","-5","-4","-3","-2","-1","0","1","2","3","4","5");
     private static final String SKIP_SIGN = "?";
-    private static final int UNSCORED = 0;
-    private static final int SCORED = 1;
 
     // Program logic
     private File corpusFile;
     private int currentLine = 1;
-    private Integer [] statusArray = new Integer [0];
     private boolean jumpToNext = false;
 
 	// GUI Dimensions
@@ -78,9 +53,8 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
     private JPanel bottomPanel = new JPanel();
     private JPanel infoPanel = new JPanel();
     private JPanel labelPanel = new JPanel();
-    private JButton eraseScoreButton = new JButton (eraseScoreButtonString);
     private JButton saveButton = new JButton(saveButtonString);
-    private JComboBox<String> scoreChooser = new JComboBox<String>(CommentsAnno.scoreStrings);
+    private JComboBox<String> scoreChooser = new JComboBox<>(scoreList.toArray(new String[0]));
     private JLabel info1Label = new JLabel ();
     private JLabel info2Label = new JLabel ();
     private JLabel statisticsLabel = new JLabel ();
@@ -91,11 +65,9 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
     private JCheckBox jumpToNextPairCheckbox = new JCheckBox (jumpToNextPairCheckboxString);
     private CommentsFile commentsFile;
 
-    @Override
     public void onLineChange(int lineIndex) {
         scrollPaneListModel
-                .setElementAt(commentsFile.getLines().get(scrollPaneList.getSelectedIndex()).toString(),
-                              lineIndex);
+                .setElementAt(commentsFile.getLine(scrollPaneList.getSelectedIndex()).toString().replace("\t"," "), lineIndex);
     }
 
     /**
@@ -116,7 +88,7 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
 			Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 			if (!isSelected) {
 				Color background = Color.WHITE;
-				if (commentsFile.getLines().get(index).isAnotated()){
+				if (commentsFile.getLine(index).isAnotated()){
                     background = Color.YELLOW;
                 }
 				c.setBackground(background);
@@ -150,14 +122,10 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
         corpusFile = new File (filePath);
 
         commentsFile = new CommentsFile(fileContent);
-        commentsFile.setListner(this);
 
         commentsFile.getLines().forEach(l -> scrollPaneListModel.addElement(l.toString().replace("\t"," ")));
     }
 
-    /**
-     * GUI initialization method
-     */
     private void initializeGUI () {
         // Annotation information settings
         info1Label.setHorizontalAlignment(JLabel.CENTER);
@@ -186,7 +154,6 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
         // Command buttons settings
         lowerPanel.setLayout(new FlowLayout());
         lowerPanel.add(scoreChooser);
-        lowerPanel.add(eraseScoreButton);
         lowerPanel.add(saveButton);
         lowerPanel.add(jumpToNextPairCheckbox);
 
@@ -222,7 +189,8 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
     	 */
     	scrollPaneList.addListSelectionListener(new ListSelectionListener() {
     		public void valueChanged(ListSelectionEvent arg0) {
-                setPair(scrollPaneList.getSelectedIndex());
+                setSelectedLine(scrollPaneList.getSelectedIndex());
+                updateInfo();
 				scrollPaneList.ensureIndexIsVisible(scrollPaneList.getSelectedIndex());
 			}});
 
@@ -235,16 +203,12 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
         		try {
         			int lineInd = Integer.parseInt(lineField.getText());
         			if (lineInd > 0 && lineInd <= commentsFile.linesCount())
-                		// The setPair method is called implicitly here, via the ListSelectionListener valueChanged method
+                		// The setSelectedLine method is called implicitly here, via the ListSelectionListener valueChanged method
                 		scrollPaneList.setSelectedIndex(lineInd-1);
         		}
         		catch (NumberFormatException ex) {}
         	}});
 
-        eraseScoreButton.addActionListener(new ActionListener () {
-            public void actionPerformed (ActionEvent e) {
-                //writeScore(null);
-            }});
 
         /**
          * Save the (partially) annotated corpus in its current state to the starting corpus file, which is overwritten
@@ -258,7 +222,13 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
         scoreChooser.addItemListener(e -> {
             if(e.getStateChange() == ItemEvent.SELECTED){
                 try {
-                    commentsFile.writeScore(currentLine, Integer.parseInt(e.getItem().toString()));
+                    String selectedValue = e.getItem().toString();
+                    if (selectedValue.equals(SKIP_SIGN)){
+                        commentsFile.getLine(currentLine).removeAnotation();
+                    } else {
+                        commentsFile.writeScore(currentLine, Integer.parseInt(selectedValue));
+                    }
+                    onLineChange(currentLine);
                 }catch (NumberFormatException ex){}
             }
         });
@@ -283,61 +253,40 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
             }});
     }
 
-    /**
-     * Update the scored, unscored and skipped text pairs count display
-     */
-    private void updateStatisticsLabel () {
-        statisticsLabel.setText(scoredString + commentsFile.annotatedCount() + "          " + unscoredString + commentsFile.notAnnotatedCount());
-    }
-
-    /**
-     * Update the current line info display and the annotation statistics info display
-     */
-    private void updateInfoSection() {
+    private void updateInfo() {
         info1Label.setText(lineLabelString1);
         lineField.setText(Integer.toString(currentLine+1));
         info2Label.setText(lineLabelString2 + commentsFile.linesCount());
-        updateStatisticsLabel();
+        statisticsLabel.setText(scoredString + commentsFile.annotatedCount() + "          " + unscoredString + commentsFile.notAnnotatedCount());
     }
 
-    /**
-     * Finds the first unscored text pair in the STS corpus and selects it.
-     * If there are no unscored pairs, the first skipped pair is selected.
-     * If all the pairs are annotated, the first pair is selected.
-     */
-    private void findNextPair () {
-    	updateInfoSection();
-        for (int i=0; i<statusArray.length; i++) {
-        	if (statusArray[i] == CommentsAnno.UNSCORED) {
-        		// The setPair method is called implicitly here, via the ListSelectionListener valueChanged method
-        		scrollPaneList.setSelectedIndex(i);
-        		return;
-        	}
-        }
-
-		// The setPair method is called implicitly here, via the ListSelectionListener valueChanged method
-        scrollPaneList.setSelectedIndex(0);
-    }
-
-    private void setPair(int selectedLine) {
+    private void setSelectedLine(int selectedLine) {
     	currentLine = selectedLine;
-        text1Area.setText(commentsFile.getLines().get(scrollPaneList.getSelectedIndex()).toString());
-        scoreChooser.setSelectedIndex(0);
+    	Line line = commentsFile.getLine(selectedLine);
 
-        updateInfoSection();
+    	text1Area.setText(line.getComment());
+        if(line.isAnotated()){
+            scoreChooser.setSelectedIndex(scoreList.indexOf("" + line.getScore()));
+        } else {
+            scoreChooser.setSelectedIndex(0);
+        }
     }
 
     private void saveToFile () {
+        PrintWriter fwout = null;
         try {
         	corpusFile.delete();
             corpusFile.createNewFile();
-            PrintWriter fwout = new PrintWriter (corpusFile, "UTF-8");
+            fwout = new PrintWriter (corpusFile, "UTF-8");
 
             commentsFile.getLines().forEach(fwout::println);
 
+        }
+        catch (IOException e) {
+            Logger.getLogger(CommentsAnno.class.getName()).log(Level.SEVERE, "Error during saving file: " , e);
+        } finally {
             fwout.close();
         }
-        catch (IOException e) {}
     }
 
     /**
@@ -348,17 +297,12 @@ public class CommentsAnno extends JFrame implements CommentsFile.CommentsFileLis
         this.dispose();
     }
 
-    /**
-     * Main class of the STS annotation program
-     * @throws IOException
-     */
-    public CommentsAnno() throws IOException {
 
+    private CommentsAnno() throws IOException {
     	String corpusFilePath = selectInputFile();
     	readCorpusFileData(corpusFilePath);
         initializeGUI();
         addListeners();
-        findNextPair ();
     }
 
     public static void main (String [] args) {
